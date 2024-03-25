a6a5dabad0653c31e782c889c626ccf74b2615dd
bhumi parmar <143687461+parmar-bhumi@users.noreply.github.com>
2024-03-25 11:08:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import  Tag from './Tag.js';
+import  Stat from './Stat.js';
+
+function App(props) {
+  return (
+    <div className="App">
+      <div style={{}}>
+      <Tag post="472" followers="12.5k" following="224"   />
+      <div>
+      <Stat  />
+      </div>
+      {/* <tag post="473"   /> */}
+
+      </div>
+      
+    </div>
+    
+    
+  );
+}
+
+
+export default App;
